256a2ec80fed586cb2afc85f7579a113641ee028
Richa0305 <richa0305@gmail.com>
2020-10-04 19:20:56 +0530

    core data layer base project

---
+++ CoreDataLayer/CoreDataLayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataLayer
+//
+//  Created by Richa Srivastava on 04/10/20.
+//  Copyright © 2020 Richa Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
